8ccb64834a1537df86f9e57b18224bf027c946b1
xelad1 <xelad1@gmail.com>
2015-04-22 19:46:17 -0500

    Cleaned up syntax errors

--- AlgorithmsPart1.js
+++ AlgorithmsPart1.js
@@ -1,50 +1,47 @@
 //Insertion sort (following code from CLRS)
 
 var insertionSort = function (array) {
-  
   var length = array.length;
+  var i, key, j;
 
-  for(var i = 2; i < length; i ++) {
+  for (i = 2; i < length; i++) {
     key = array[i];
     //Insert A[i] into the sorted sequence A[1 ... j - 1];
-    var j = i - 1;
-    while(j > 0 && array[j] > key) {
+    j = i - 1;
+    while (j > 0 && array[j] > key) {
       array[j + 1] = array[j];
       j = j - 1;
     }
     array[j + 1] = key;
-  } 
+  }
 
   return array;
-}
+};
 
 //My own insertion sort
 
 var myInsertionSort = function (array) {
-  
   var length = array.length;
+  var i, key, j;
 
   //loop through the array
-  for(var i = 1; i < length; i ++) {
+  for (i = 1; i < length; i++) {
 
-    var j = i;
-    var key = array[j];
+    j = i;
+    key = array[j];
 
-    //iterate backwards from the i valu
-    while(j > 0) {
-      if(array[j] < array[j - 1]) {
+    //iterate backwards from the i value
+    //    
+    while (j > 0) {
+      if (array[j] < array[j - 1]) {
         array[j] = array[j - 1];
         array[j - 1] = key;
       }
       j--;
     }
-  }   
+  }
   return array;
-}
-
-//54321
-//45321
-//
+};
 
 //Merge sort
 
@@ -52,9 +49,10 @@ var mergeSort = function  (array) {
   
   var i, j = 1;
   var n = null;
+  var k;
 
-  for(var k = 0; k < n; k ++) {
+  for (k = 0; k < n; k ++) {
 
-    if()
+    if ()
   }
-}
+};
